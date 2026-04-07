898b36149ebea3a8b6d00f3701e010b73a2e91d0
Jonathan Maita <jonamaita@gmail.com>
2026-04-07 14:57:33 -0400

    refactor: Update setup for 'tree-sitter' plugin

--- common/.config/nvim/lua/plugins/treesitter.lua
+++ common/.config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,49 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    lazy = false,
     config = function ()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        ensure_installed = { "lua", "vim", "query", "javascript", "html", "go", "python", "dockerfile", "bash", "yaml"},
-        sync_install = false,
-        highlight = {
-          enable = true,
-       },
-        indent = { enable = true },
-    })
+        local treesitter = require("nvim-treesitter")
+        treesitter.setup({
+            install_dir = vim.fs.joinpath(vim.fn.stdpath('data'), 'treesitter'),
+        })
+
+        treesitter.install({
+            'bash',
+            'c',
+            'cpp',
+            'css',
+            'dockerfile',
+            'fish',
+            'gitignore',
+            'go',
+            'gomod',
+            'graphql',
+            'html',
+            'javascript',
+            'json',
+            'kdl',
+            'lua',
+            'make',
+            'markdown',
+            'meson',
+            'ninja',
+            'php',
+            'python',
+            'rasi',
+            'ruby',
+            'rust',
+            'scss',
+            'sql',
+            'sxhkdrc',
+            'toml',
+            'tsx',
+            'twig',
+            'typescript',
+            'vue',
+            'yaml',
+            -- 'help', -- Broken for some reason
+            'comment',
+        })
     end
 }
